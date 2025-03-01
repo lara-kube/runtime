7b7d986f475b60c5f2a482422440f5529eeb4c37
Francisco Barrento <francisco.barrento@gmail.com>
2025-03-01 20:28:48 +0000

    fixes(nginx): the closing bracket in PHP_FPM_HOST variable is missing in /etc/nginx/conf.d/default.conf:54

--- nginx-app-sidecar/entrypoint.sh
+++ nginx-app-sidecar/entrypoint.sh
@@ -5,4 +5,8 @@ echo "Using NGINX_ROOT=${NGINX_ROOT}"
 echo "Using PHP_FPM_HOST=${PHP_FPM_HOST:-app}"
 echo "Using PHP_FPM_PORT=${PHP_FPM_PORT:-9000}"
 
-exit 0
+# Process the template - using double quotes to allow variable expansion
+envsubst "\${NGINX_ROOT} \${PHP_FPM_HOST} \${PHP_FPM_PORT}" < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
+
+# Start Nginx
+exec "$@"
